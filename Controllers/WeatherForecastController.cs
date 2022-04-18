8103bf71a8e741e4bb04c5fdd2bf4ae0d9bd8aeb
Sandeep Pabbu <sandeep.pabbu@gmail.com>
2022-04-18 18:33:50 +0530

    Added method GetWeather by City

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -29,5 +29,17 @@ namespace gitwebapidemo.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet(Name = "GetWeatherForecastByCity")]
+        public IEnumerable<WeatherForecast> GetWeatherByCity()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
